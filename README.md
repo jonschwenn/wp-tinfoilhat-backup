777cd64bab3f3a3296159723a2c1db62d46605e0
jschwenn <jschwenn@digitalocean.com>
2018-03-23 16:10:28 -0500

    Initial Commit - WordPress Backup Script

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# wp-tinfoilhat-backup
-Yo dawg, I heard you like backups. So I made a script that puts your backups in your backups.
+# A.B.B.
+Always be backing-up.  
+
+Let's imagine you have a WordPress site running on a server. You want to backup your WordPress site because it's important to you and you'd hate to loose posts, images, or even worse - customer orders if you use WP for e-commerce. There are many ways to back it up and most of those include plugins which are simple to use.
+
+Instead of taking a backup using a WordPress plugin which lives inside of WordPress, here is a basic script that is configured to do a daily backup of the WordPress sites and copy them to multiple locations as well as keeping a set number of historical copies locally on the server.
+
+### Prerequisites
+- WordPress running on a LAMP (Linux, Apache, MySQL, PHP) server
+- Populate your username and password for MySQL in ~/.my.cnf
+- s3cmd installed and configured
+ - DigitalOcean Spaces is a great low-cost option for Object storage.
+ - [Here is a tutorial|https://www.digitalocean.com/community/tutorials/how-to-configure-s3cmd-2-x-to-manage-digitalocean-spaces] on how to install s3cmd and configure it for DigitalOcean Spaces
+- A remote host configured with SSH Key authentication if a remote host destination is used
+
+### Setup
+- Copy the script file or script contents to your server
+- Set the variables in the script file
+- Make your script executable
+ - sudo chmod a+x /root/wp-backup.sh
+- Add a crontab entry to automate the script
+ - This example would run the script at 4am server time daily:
+```
+$crontab -e
+
+0 4 * * * /root/wp-backup.sh >/dev/null 2>&1
+```
+
+_Periodically clearing out old backups will be needed for a Remote Host and/or Object Storage bucket.  That's another script._
---
+++ wp-backup.sh
@@ -0,0 +1,112 @@
+#!/usr/bin/env bash
+# Script to backup WordPress to multiple locations
+# Read the README.md to ensure proper configuration
+# https://github.com/jonschwenn/wp-tinfoilhat-backup
+set -eo pipefail
+readonly SCRIPT_NAME=$(basename $0)
+
+# SET THESE VARIABLES
+# ---------------------------------------------------------------------
+# Directory for all backups to be stored locally
+BACKUP_DIR=/root/backups
+# Directory for WordPress
+# Only single WordPress instance is allowed in this version
+WP_DIR=/var/www/html
+# How many local copies should be saved (in days)
+COPIES=14
+# S3 Bucket location
+# Leave Blank to disable S3 Sync
+S3CMD_BUCKET=
+# Remote Host (SSH Keys need to be configured)
+# Leave Blank to disable Remote Host
+REMOTE_HOST=
+REMOTE_USER=root
+REMOTE_PORT=22
+REMOTE_PATH=/root/www-backups
+# ---------------------------------------------------------------------
+
+get_db_name (){
+WP_CONFIG=$(find $WP_DIR -name wp-config.php)
+DB_NAME=$(grep DB_NAME $WP_CONFIG | cut -d \' -f 4)
+}
+
+check_s3cmd (){
+echo "TODO: Prevalidate s3cmd"
+
+}
+
+check_remotehost(){
+echo  "TODO: Prevalidate remote host connection"
+}
+
+create_backup (){
+# Setup Backup Directory
+cd $BACKUP_DIR
+mkdir backup-$(date +%Y-%m-%d)
+cd backup-$(date +%Y-%m-%d)
+
+# Backup Database
+get_db_name "$@"
+mysqldump --opt -Q $DB_NAME > database-$(date +%Y-%m-%d).sql || \
+err "Database Backup Failed"
+
+# Backup Site Files
+tar -czf site-$(date +%Y-%m-%d).tar.gz $WP_DIR || \
+err "Site File Backup Failed"
+
+# Create Final Backup File
+cd $BACKUP_DIR
+tar -cf backup-$(date +%Y-%m-%d).tar backup-$(date +%Y-%m-%d) && \
+log "Backup backup-$(date +%Y-%m-%d).tar Created" || \
+err "Backup Process Failed"
+}
+
+remote_sync (){
+
+scp -P$REMOTE_PORT $BACKUP_DIR/backup-$(date +%Y-%m-%d).tar $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH && \
+log "Backup Sent to Remote Host" || \
+err "Remote Host Process Failed"
+}
+
+s3cmd_sync (){
+
+# Send copy to S3cmd configured object storage
+s3cmd put $BACKUP_DIR/backup-$(date +%Y-%m-%d).tar s3://$S3CMD_BUCKET  && \
+log "Backup Sent to Object Storage" || \
+err "Object Storage Process Failed"
+}
+
+cleanup (){
+# Cleanup Backup Working Directory
+cd $BACKUP_DIR
+rm -rf backup-$(date +%Y-%m-%d)
+# Cleanup old copies
+if [ $(ls -t | sed -e '1,'"$COPIES"'d' | wc -l) -ge  1  ]
+then
+ls -t | sed -e '1,'"$COPIES"'d' | xargs -d '\n' rm && \
+log "Cleanup Process for Backups older than $COPIES days completed"
+else
+log "Cleanup Process found backups older than $COPIES days "
+fi
+
+}
+
+log() {
+  echo "$@"
+  logger -p user.notice -t $SCRIPT_NAME "$@"
+}
+
+err() {
+  echo "$@" >&2
+  logger -p user.error -t $SCRIPT_NAME "$@"
+}
+
+main() {
+  get_db_name &&
+  create_backup &&
+  remote_sync &&
+  s3cmd_sync &&
+  cleanup
+}
+
+main "$@"
